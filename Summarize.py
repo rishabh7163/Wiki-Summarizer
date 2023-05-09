d721eaa2b5a0ebc5bdd9c7f848f3a50c077a9846
rishabh7163 <40897005+rishabh7163@users.noreply.github.com>
2023-05-08 19:52:28 -0700

    Update Summarize.py
    Using "LexRankSummarizer" instead of "LuhnSummarizer"

--- Summarize.py
+++ Summarize.py
@@ -1,29 +1,32 @@
 import wikipedia
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.luhn import LuhnSummarizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
 
 import sys
 if sys.stdout.encoding != 'utf-8':
     sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf-8', buffering=1)
 
 # Fetching the Wikipedia page
-page = wikipedia.page("Enter_the_Wiki_Topic_Here")
+page = wikipedia.page("Hyperspectral Imaging")
 
 # Creating a plaintext parser
 parser = PlaintextParser.from_string(page.content, Tokenizer('english'))
 
-# Creating a summarizer
-summarizer = LuhnSummarizer()
+# Creating a summarizer with a lower similarity threshold
+summarizer = LexRankSummarizer()
+summarizer.similarity_threshold = 0.1
 
-# Calculate the number of sentences in the text
-total_sentences = len(list(parser.document.sentences))
+# Generating a summary for each paragraph
+for paragraph in parser.document.paragraphs:
+    # Generating a summary
+    summary_length = 0.3  # Summary length as a fraction of the original text
+    sentence_count = max(int(len(paragraph.sentences) * summary_length), 1)  # Ensure at least one sentence is included
+    summary = summarizer(paragraph, sentence_count)
 
-# Generating a summary
-summary_length = 0.3  # Summary length as a fraction of the original text
-sentence_count = max(int(total_sentences * summary_length), 1)  # Ensure at least one sentence is included
-summary = summarizer(parser.document, sentences_count=sentence_count)
-
-# Printing the summary
-for sentence in summary:
-    print(sentence)
+    # Printing the original paragraph and its summary
+    print("\nORIGINAL PARAGRAPH:")
+    print(paragraph)
+    print("SUMMARY:")
+    for sentence in summary:
+        print(sentence)
